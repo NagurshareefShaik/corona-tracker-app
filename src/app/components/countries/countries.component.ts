6d7ec1d9c676fb7a62f48c35f6428eeb999a55db
Nagurshareef <nagurshareef123@gmail.com>
2020-11-08 21:40:36 +0530

    charts commit for data

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -9,6 +9,10 @@ import { DataServiceService } from 'src/app/service/data-service.service';
   styleUrls: ['./countries.component.css']
 })
 export class CountriesComponent implements OnInit {
+  totalConfirmed=0;
+  totalActive=0;
+  totalDeaths=0;
+  totalRecovered=0;
 countries:string[]=[];
 data :GlobalDataSummary[];
   constructor(private dtaService:DataServiceService) { }
@@ -17,9 +21,23 @@ data :GlobalDataSummary[];
     this.dtaService.getGlobalData().subscribe(result=>{
       this.data=result;
       this.data.forEach(cs=>{
+        if(!Number.isNaN(cs.confirmed)){
         this.countries.push(cs.country);
+        }
       })
+      this.updateValues(this.data[0]['country'])
     })
+  }
+
+  updateValues(country){
+      this.data.forEach(cs=>{
+        if(country==cs.country){
+          this.totalConfirmed=cs.confirmed;
+          this.totalRecovered=cs.recovered;
+          this.totalDeaths=cs.deaths;
+          this.totalActive=cs.active;
+        }
+      })
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/service/data-service.service';
+import { GoogleChartInterface } from 'ng2-google-charts';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +9,62 @@ import { DataServiceService } from 'src/app/service/data-service.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  globalData:GlobalDataSummary[]=[];
   totalConfirmed=0;
   totalActive=0;
   totalDeaths=0;
   totalRecovered=0;
+  pieChart: GoogleChartInterface={
+    chartType: 'PieChart'
+  };
+  columnChart: GoogleChartInterface={
+    chartType: 'ColumnChart'
+  };
   constructor(private dataService:DataServiceService) { }
+
+initChart(caseType:string){
+  let dataTable=[];
+  dataTable.push(["Country","Cases"])
+  this.globalData.forEach(cs=>{
+    let value:number;
+      if(caseType == 'a'){
+        value=cs.active
+      }
+      if(caseType == 'c'){
+        value=cs.confirmed
+      }
+      if(caseType == 'r'){
+        value=cs.recovered
+      }
+      if(caseType == 'd'){
+        value=cs.deaths
+      }
+    dataTable.push([cs.country,value])
+  })
+  console.log(dataTable);
+  
+  this.pieChart= {
+    chartType: 'PieChart',
+    dataTable: dataTable,
+    //firstRowIsData: true,
+    options: {
+      height:400
+    },
+  };
+  this.columnChart= {
+    chartType: 'ColumnChart',
+    dataTable: dataTable,
+    //firstRowIsData: true,
+    options: {
+      height:400
+    },
+  };
+}
 
   ngOnInit(): void {
     this.dataService.getGlobalData().subscribe({
       next :(result)=>{
+        this.globalData=result;
         result.forEach(cs => {
           if(!Number.isNaN(cs.confirmed)){
           this.totalConfirmed +=cs.confirmed;
@@ -24,8 +73,13 @@ export class HomeComponent implements OnInit {
           this.totalRecovered +=cs.recovered;
         }
         })
+        this.initChart('c');
       }
     })
   }
 
+  updateChart(input:HTMLInputElement){
+    let caseType:string=input.value;
+    this.initChart(caseType);
+  }
 }
